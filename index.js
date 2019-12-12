4984abd12316f0a052ba8c27f2a19ca4b7047ea8
smv5047 <smv5047@gmail.com>
2019-12-11 20:48:55 -0500

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express")
 
+//use this
+const dotenv = require("dotenv")
+dotenv.config()
+
+//or in the package.json script include the below
+//requires the dotenv/config module before index,.js starts
+//"server": "node or nodemon -r dotenv/config index.js"
+
 const app = express()
 const host = "0.0.0.0"
 const port = 8080
@@ -12,6 +20,8 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
 	res.json({
 		message: "Welcome to our API",
+		cohort: process.env.LAMBDA_COHORT,
+		secret: process.env.SUPER_SECRET_API_KEY,
 	})
 })
 
