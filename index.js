80d5c3ae6d811a8dba2ef43f14ce68996297e019
smv5047 <smv5047@gmail.com>
2019-12-11 21:05:54 -0500

    more better

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express")
 
 //use this
-const dotenv = require("dotenv")
-dotenv.config()
+// const dotenv = require("dotenv")
+// dotenv.config()
 
 //or in the package.json script include the below
 //requires the dotenv/config module before index,.js starts
 //"server": "node or nodemon -r dotenv/config index.js"
 
 const app = express()
-const host = "0.0.0.0"
-const port = 8080
+const host = process.env.HOST || "0.0.0.0"
+//Port would have to be 443 (HTTPS) or 80 (HTTP)
+//HEROKU assigns your port
+const port = process.env.PORT || 8080
 
 app.use((req, res, next) => {
 	console.log(`[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url}`)
